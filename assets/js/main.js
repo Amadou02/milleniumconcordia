fb2438602e8374625f240ef7e607cd45248d221a
Hassan <derkaouihassan19@gmail.com>
2019-05-15 23:04:50 +0200

    ajout d'un panier avec des boutons

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,6 +15,16 @@ function rafraichirPanier() {
   var nbreArticle1 = 0;
   var nbreArticle2 = 0;
   var nbreArticle3 = 0;
+  var nbreArticle4 = 0;
+  var nbreArticle5 = 0;
+  var nbreArticle6 = 0;
+  var nbreArticle7 = 0;
+  var nbreArticle8 = 0;
+  var nbreArticle9 = 0;
+  var nbreArticle10 = 0;
+  var nbreArticle11 = 0;
+  var nbreArticle12 = 0;
+
 
   for(var i = 0; i < panier.length; i++) {
     if(panier[i] == 1) {
@@ -31,6 +41,51 @@ function rafraichirPanier() {
       nbreArticle3++;
       document.getElementById('article3').innerHTML = 'Vous avez ' + nbreArticle3 + ' articles 3';
       document.getElementById('article3').style.visibility = 'visible';
+    }
+    if(panier[i] == 100) {
+      nbreArticle4++;
+      document.getElementById('article4').innerHTML = 'Vous avez ' + nbreArticle4 + ' articles 4';
+      document.getElementById('article4').style.visibility = 'visible';
+    }
+    if(panier[i] == 100) {
+      nbreArticle5++;
+      document.getElementById('article5').innerHTML = 'Vous avez ' + nbreArticle5 + ' articles 5';
+      document.getElementById('article5').style.visibility = 'visible';
+    }
+    if(panier[i] == 100) {
+      nbreArticle6++;
+      document.getElementById('article6').innerHTML = 'Vous avez ' + nbreArticle6 + ' articles 6';
+      document.getElementById('article6').style.visibility = 'visible';
+    }
+    if(panier[i] == 100) {
+      nbreArticle7++;
+      document.getElementById('article7').innerHTML = 'Vous avez ' + nbreArticle7 + ' articles 7';
+      document.getElementById('article7').style.visibility = 'visible';
+    }
+    if(panier[i] == 100) {
+      nbreArticle8++;
+      document.getElementById('article8').innerHTML = 'Vous avez ' + nbreArticle8 + ' articles 8';
+      document.getElementById('article8').style.visibility = 'visible';
+    }
+    if(panier[i] == 100) {
+      nbreArticle9++;
+      document.getElementById('article9').innerHTML = 'Vous avez ' + nbreArticle9 + ' articles 9';
+      document.getElementById('article9').style.visibility = 'visible';
+    }
+    if(panier[i] == 100) {
+      nbreArticle10++;
+      document.getElementById('article10').innerHTML = 'Vous avez ' + nbreArticle10 + ' articles 10';
+      document.getElementById('article10').style.visibility = 'visible';
+    }
+    if(panier[i] == 100) {
+      nbreArticle11++;
+      document.getElementById('article11').innerHTML = 'Vous avez ' + nbreArticle11 + ' articles 11';
+      document.getElementById('article11').style.visibility = 'visible';
+    }
+    if(panier[i] == 100) {
+      nbreArticle12++;
+      document.getElementById('article12').innerHTML = 'Vous avez ' + nbreArticle12 + ' articles 12';
+      document.getElementById('article12').style.visibility = 'visible';
     }
   }
 }
